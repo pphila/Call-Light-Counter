14cad1f165d7402dc8c50d541da21fb283dc9284
pphila <efl2ice89@gmail.com>
2024-06-11 19:29:15 -0700

    add states to setting date, shift, and counter to main component

--- src/components/MainCounter.js
+++ src/components/MainCounter.js
@@ -17,5 +17,32 @@ const MainCounter = () => {
     newCounters[index] -= 1;
     setCounters(newCounters);
   };
-  
+
+  return(
+    <div>
+      <h1>Call Light Counter</h1>
+      <div>
+        <label>
+          Date:
+          <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+        </label>
+        <label>
+          Shift:
+          <input type="date" value={shift} onChange={(e) => setShift(e.target.value)} />
+        </label>
+      </div>
+      <div className='grid'>
+        {counters.map((count, index) => (
+          <div key={index} className='counter-box'>
+            <span>Room {index + 1}</span>
+            <button onClick={() => incrementCounter(index)}>+</button>
+            <span>{count}</span>
+            {count > 0 && <button onClick={() => decrementCounter(index)}>-</button>}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default MainCounter;
