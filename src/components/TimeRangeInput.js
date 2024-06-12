33f67413995994d784b500fb24175926e8c9e2e9
pphila <efl2ice89@gmail.com>
2024-06-11 19:49:30 -0700

    add code to time range component

--- src/components/TimeRangeInput.js
+++ src/components/TimeRangeInput.js
@@ -1,1 +1,31 @@
 import React from 'react';
+
+const TimeRangeInput = ({shift, setShift}) => {
+  const [inputValue, setInputValue] = useState(shift);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+
+    const regex = /^([01]\d|2[0-3])[0-5]\d-([01]\d|2[0-3])[0-5]\d$/;
+    if (regex.test(value)) {
+      setShift(value);
+    }
+  };
+
+  return(
+    <div>
+      <label>
+        Shift (HHMM-HHMM):
+        <input 
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder='0000-0000'
+        />
+      </label>
+    </div>
+  );
+};
+
+export default TimeRangeInput;
